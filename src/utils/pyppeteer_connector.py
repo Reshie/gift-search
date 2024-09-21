33cdb878b12642255b5e01d2029590ea73183194
Reshie <reo040125@gmail.com>
2024-09-21 16:10:27 +0900

    Create pyppeteer_connector.py

---
+++ src/utils/pyppeteer_connector.py
@@ -0,0 +1,32 @@
+from pyppeteer import launch
+from pyppeteer.browser import Browser
+from pyppeteer.page import Page
+
+class PyppeteerConnector:
+    async def init(self):
+        self.browser: Browser = await launch()
+
+    async def get(self, url):
+        try:
+            self.page: Page = await self.browser.newPage()
+            await self.page.goto(url)
+        except Exception as e:
+            print(f"Network error: {e}")
+
+    async def get_response_body(self, request_id):
+        try:
+            session = await self.page.target.createCDPSession()
+            response = await session.send(
+                'Network.getResponseBody',
+                {'requestId': request_id}
+            )
+            await session.detach()
+            return response
+        except Exception as e:
+            print(f"Failed to get response: {e}")
+    
+    async def find_elements(self, class_name: str):
+        return await self.page.querySelectorAll(class_name)
+        
+    async def quit(self):
+        await self.browser.close()
